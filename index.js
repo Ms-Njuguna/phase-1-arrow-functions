b512a5aa68bfddb3bc5f5338f3367494573f24b4
Patricia <patricianjuguna084@gmail.com>
2025-06-04 17:35:24 +0300

    Worked on arrow functions and function expressions

---
+++ index.js
@@ -0,0 +1,19 @@
+//created a divide function expression called divide
+const divide = function(a,b) {
+    return a / b;
+}
+
+divide(2000,100);
+console.log(divide(2000,100));
+
+//created an arrow function called square
+const square = num1 => num1 * num1;
+
+square(3);
+console.log(square(3));
+
+//created an arrow function called add
+const add = (a,b) => a + b;
+
+add(3,4);
+console.log(add(3,4));
